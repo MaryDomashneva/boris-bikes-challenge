45f404be72f800d839304024c7adb18fe12aa589
marydomashneva <mary.domashneva@gmail.com>
2018-03-21 22:32:51 +0000

    Set up a capacuty as argument and pre-defined capacity if use does not pass argument

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,20 @@
 class DockingStation
+  INITIAL_BIKES = 20
+
   attr_reader :bikes_storage
 
-  def initialize
-    @bikes_storage = []
-    20.times do return_bike(Bike.new)
+  def initialize(capacity = INITIAL_BIKES, initial_bikes_storage = nil)
+    if initial_bikes_storage.nil?
+      @bikes_storage = []
+      capacity.times { @bikes_storage << Bike.new }
+    else
+      @bikes_storage = initial_bikes_storage
     end
-    return @bikes_storage
+    @capacity = capacity
   end
 
   def release_bike
-    if @bikes_storage.empty?
+    if empty?
       raise "No bikes!"
     else
       released_bike = Bike.new
@@ -17,11 +22,27 @@ class DockingStation
   end
 
   def return_bike(bike)
-    if @bikes_storage.count >= 21
+    if full?
       raise "Docking station is full!"
     else
       @bikes_storage << bike
     end
+  end
+
+  private
+
+  attr_reader :capacity
+
+  def full?
+    if @bikes_storage.count == @capacity
+      return true
+    else
+      return false
+    end
+  end
+
+  def empty?
+    @bikes_storage.empty?
   end
 
 end
